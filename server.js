b18a42b1455b5bd88fdaf05953c82ab4c02f9610
PauloSilvaRamalho <phsilvarm@gmail.com>
2025-11-03 15:01:12 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-//1. Import as bibliotecas 
+// 1. Import as bibliotecas 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//2. configurar o servidor Express
+// 2. configurar o servidor Express
 const app = express();
-app.use(cors());//permite que o front end acesse este back end
-app.use(express.json());//Permite que o servidor entenda JSON
+app.use(cors()); //Permite que o front end acesse este back-end
+app.use(express.json()); // Permite que o servidor entenda JSON
 
-//3. conectar ao mongoDB
-const MongoURI = ''; //clolocar a url do seu projeto no mongoDB
-mongoose.connect(MongoURI, {useNewParser: true, useUnifieldTopology: true})
-.then(() => console.log('Conectado ao MongoDB com sucesso!'))
-.catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+// 3. conectar ao mongoDb
+const MongoURI = 'mongodb://localhost:27017/relatorio_atividades'; //colocar a url do seu projeto no mongoDB
+mongoose.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true }) // corrigi aqui
+    .then(() => console.log('Conectado ao MongoDB com sucesso!'))
+    .catch(err => console.error('Erro ao conectar ao MongoDB: ', err));
 
 //4. Definir o "Schema" - A estrutura dos seus dados
-
-//Isso deve corresponder à estrutura do seu formulário
+// Isso deve corresponder à estrutura do seu formulário
 const relatorioSchema = new mongoose.Schema({
     titulo: String,
     tipo: String,
@@ -30,16 +29,17 @@ const relatorioSchema = new mongoose.Schema({
     },
     palavras_chave: [String],
     revisoes: [{
+        data: Date,
         revisado_por: String,
         comentario: String
     }]
 });
 
-//5. Criar o "Model" - O objeto que representa sua coleção no banco
+// 5. Criar o "Model" - O objeto que representa sua coleção no banco
 const Relatorio = mongoose.model('Relatorio', relatorioSchema);
 
-//6. Criar a "Rota" ou "Endpoint" -o URL que o front-end irá chamar
-app.post('./salvar-relatorio', async(req, res) =>{
+// 6.Criar a "Rota" ou "Endpoint" - o URL que o front-end irá chamar
+app.post('/salvar-relatorio', async(req, res) =>{ //arrumei aqui
     try{
         //Pega os dados que o front-end enviou (estão em req.body)
         const dadosDoFormulario = req.body;
@@ -50,19 +50,18 @@ app.post('./salvar-relatorio', async(req, res) =>{
         // Salva o documento no banco de dados
         await novoRelatorio.save();
 
-        // Envia uma reposta de sucesso de volta para o front-end
+        // Envia uma resposta de sucesso de volta para o front-end
         res.status(201).json({message: 'Relatório salvo com sucesso!'});
         console.log('Relatório salvo: ', novoRelatorio.titulo);
     }catch(error){
         // se der erro, envia uma mensagem de erro
-        res.status(500).json({ message: 'Ocorreu um erro ao salvar o relatório.',
-            erro: error});
-            console.error('Erro ao salvar: ',error);
+        res.status(500).json({ message: 'Ocorreu um erro ao salvar o relatório.', error: error});
+        console.error('Erro ao salvar: ', error);
     }
 });
 
 // 7 Iniciar o servidor.
 const PORT = 3000;
-app.listenerCount(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor back-end rodando na porta ${PORT}`);
 });
